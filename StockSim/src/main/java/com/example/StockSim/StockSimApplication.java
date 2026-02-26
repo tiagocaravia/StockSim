7913468f2aa6cada2f61ea2cb0f69e57b5793604
Santiago Caravia Perry <tiagocperry@gmail.com>
2026-02-26 00:08:14 -0700

    SpringBoot Real Add

---
+++ StockSim/src/main/java/com/example/StockSim/StockSimApplication.java
@@ -0,0 +1,13 @@
+package com.example.StockSim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockSimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockSimApplication.class, args);
+	}
+
+}
---
+++ StockSim/src/test/java/com/example/StockSim/StockSimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StockSim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
